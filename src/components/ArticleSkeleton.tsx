0889d668961a43a9240a4a70709ed108f0241ddb
Viktor Dimitrov <viktor.dimitrov.dev@gmail.com>
2025-07-19 11:10:26 +0300

    Add ArticleSkeleton component for loading state representation

---
+++ src/components/ArticleSkeleton.tsx
@@ -0,0 +1,10 @@
+const ArticleSkeleton = () => (
+  <div className="py-6 border-b border-zinc-700/50 animate-pulse">
+    <div className="h-3 bg-zinc-700 rounded w-1/4 mb-3"></div>
+    <div className="h-6 bg-zinc-600 rounded w-3/4 mb-3"></div>
+    <div className="h-4 bg-zinc-700 rounded w-full mb-1"></div>
+    <div className="h-4 bg-zinc-700 rounded w-5/6"></div>
+  </div>
+);
+
+export default ArticleSkeleton;
